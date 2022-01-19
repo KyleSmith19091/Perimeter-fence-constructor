448e95f9115f4e9ef5e86d65c1a3a9f8d9f837c3
Kyle Smith <sKorpion19091@gmail.com>
2022-01-19 11:27:16 +0200

    Added comments to the fence manager class

--- fenceManager.js
+++ fenceManager.js
@@ -1,14 +1,23 @@
+// Fence Manager
 class FenceManager {
+
 	constructor() {
+        // Points represent fence posts
 		this.points = [];
+
+        // First post/point to be place
 		this.genesisPoint = undefined;
+
+        // Convex hull / Perimeter created by fences
 		this.convexHull = new ConvexHull();
 	}
 
+    // Setup the genesis point
 	setGenesisPoint(x,y) {
 		this.genesisPoint = new GenesisPoint(x,y);
 	}
 
+    // Create virtual plane from genesis point if set
 	setupVirtualPlane() {
 		if(this.genesisPoint) {
 			line(0, this.genesisPoint.y, WIDTH, this.genesisPoint.y); // line (y = 0)
@@ -16,6 +25,7 @@ class FenceManager {
 		} 
 	}
 
+    // Create the Fence Boundary 
 	constructFenceBoundary() {
 		if(this.points.length > 2) {
 			this.setupVirtualPlane();
@@ -24,6 +34,7 @@ class FenceManager {
 		}
 	}
 
+    // Adds fence post to the post collection
 	addFencePost(x,y) {
 		if(this.points.length === 0) {
 			console.log("Added Gensis Point");
